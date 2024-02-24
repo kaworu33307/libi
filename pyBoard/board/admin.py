618dd49976d9ab7d6a875ecbbbe19132886773f0
kaworu33307 <timurshimshek93@gmail.com>
2024-02-24 17:28:36 +0300

    +

--- pyBoard/board/admin.py
+++ pyBoard/board/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author,Owner,Book
+admin.site.register(Author)
+admin.site.register(Owner)
+admin.site.register(Book)
--- pyBoard/board/models.py
+++ pyBoard/board/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    firstAuthorName = models.CharField(verbose_name="Имя автора", max_length=250)
+    lastAuthorName = models.CharField(verbose_name="Фамилия автора",max_length=250)
+    def __str__(self):
+        return self.lastAuthorName
+class Owner(models.Model):
+      firstOwnerName = models.CharField(verbose_name="Имя владельца", max_length=250)
+      lastOwnerName = models.CharField(verbose_name="Фамилия владельца", max_length=250)
+
+      def __str__(self):
+          return self.lastOwnerName
+class Book(models.Model):
+    title = models.CharField(verbose_name="Название книги", max_length=250)
+    description = models.TextField(verbose_name="Описание книги")
+    author = models.ForeignKey(Author, verbose_name="Автор книги", on_delete=models.CASCADE, related_name='Author')
+    owner = models.ForeignKey(Owner, verbose_name="Владелец книги", on_delete=models.CASCADE, related_name='Owner')
+    img = models.ImageField()
+    def __str__(self):
+        return self.title
